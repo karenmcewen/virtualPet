61c4612db5dc55864a0eb75e63ce21d873b06c59
karenmcewen <karenf_email@yahoo.com>
2017-09-07 21:04:36 -0400

    added class MiPodi

---
+++ virtualPet/MiPodi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualPet
+{
+    class MiPodi
+    {
+        //FIELDS
+
+        private string name;
+        private string color;
+        private int numPods;
+        private int currentEnergy;
+        private bool isHappy;
+
+        //PROPERTIES
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public int NumPods
+        {
+            get { return this.numPods; }
+        }
+        public int CurrentEnergy
+        {
+            get { return this.currentEnergy; }
+        }
+
+        public bool IsHappy
+        {
+            get { return this.isHappy; }
+        }
+
+        // CONSTRUCTORS
+        public MiPodi()
+        {
+            this.name = "Midgie";
+            this.color = "red";
+            this.numPods = 1;
+            this.currentEnergy = 10;
+            this.isHappy = true;
+            
+        }
+
+        public MiPodi(string name)
+        {
+            this.name = "Midgie";
+            this.color = "red";
+            this.numPods = 1;
+            this.currentEnergy = 10;
+            this.isHappy = true;
+
+        }
+        /////////// 
+    }
+}
