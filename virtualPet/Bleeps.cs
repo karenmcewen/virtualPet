f5969b8a3e2ad9fc681bf3af092be42693a621f3
karenmcewen <karenf_email@yahoo.com>
2017-09-08 16:31:44 -0400

    deleted miPodi, created new class called Bleeps

---
+++ virtualPet/Bleeps.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtualPet
+{
+    class Bleeps
+    {
+        //FIELDS (at least 3 required)
+
+        private string name;
+        private int currentNumBuds; //number of buds each Bleep currently has (every Bleep has exactly the same)
+        private int currentEnergy; //amount of energy each Bleep currently has 
+        private int energyToBud; //amount of energy needed to grow a new bud                      
+        private bool isHappy; //becomes true if played with regularly.  
+        private bool isAnnoyed; //if you annoy them, they'll Blick Bowl you
+
+          
+
+        //PROPERTIES  (at least 3 required)
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }//allows user to choose a name
+        }
+
+       
+        public int CurrentNumBuds
+        {
+            get { return this.currentNumBuds; }
+            //no set because user does not have input into this
+        }
+        public int CurrentEnergy
+        {
+            get { return this.currentEnergy; }
+            //no set because user does not have input into this
+        }
+
+        public int EnergyToBud
+        {
+            get { return this.energyToBud; }
+            //no set because user does not have input into this
+        }
+        public bool IsHappy
+        {
+            get { return this.isHappy; }
+            //no set because user does not have input into this
+        }
+
+        public bool IsAnnoyed
+        {
+            get { return this.isAnnoyed; }
+            //no set because user does not have input into this
+        }
+
+        // CONSTRUCTORS (at least 1 required)
+        public Bleeps()//default - no user input
+        {
+            this.name = "Blobbi"; //default name
+            this.currentNumBuds = 0;
+            this.currentEnergy = 10;
+            this.isHappy = true;
+            this.isAnnoyed = false;
+
+        }
+
+        public Bleeps(string name)//user chooses name
+        {
+            this.name = name; //user inputs a name
+            this.currentNumBuds = 0;
+            this.currentEnergy = 10;
+            this.isHappy = false;
+            this.isAnnoyed = false;
+
+        }
+
+        //NTS THIS is an advanced option for later...
+        public MiPodi(string name, int podiType)//user chooses name & type
+        {
+            this.name = name; //user inputs a name
+            this.podiType = podiType; //user chooses a type
+            this.currentNumPods = 0;
+            this.currentEnergy = 10;
+            this.isHappy = false;
+
+        }
+
+        //METHODS (at least 3 required)
+
+        public void Eat()
+        {
+
+        }
+
+        public void Play()
+        {
+
+        }
+
+        public void Podulate()
+        {
+
+        }
+
+        public void Popate()
+        {
+
+        }
+
+        /////////// here there be dragons - don't delete these 2 curly brackets!
+    }
+}
--- virtualPet/Program.cs
+++ virtualPet/Program.cs
@@ -10,20 +10,32 @@ namespace virtualPet
     {
         static void Main(string[] args)
         {
+            //Requirements - Main method
+            //Interactive user interface (see example at bottom of page)
+            //Display current status of pet
+            //Display options for interacting with pet
+            //Ask user what action to take
+            //User's selection should trigger an action
+
+
             //Introduction 
-            Console.WriteLine("Welcome to Planet PodLandia!");
-            Console.WriteLine("Sadly, the planet's inhabitants, the Podi, have petered out.");
-            Console.WriteLine("Only a single, sad and sickly podless Podus remains.");  
-            
+            Console.WriteLine("Welcome to Planet X");
+            Console.WriteLine("Sadly, the planet's inhabitants, the Bleeps, have become nearly extinct.");
+            Console.WriteLine("Only a single, sad and sickly Bloop remains.");
+
             //NTS-move this section to a separate thing that can be accessed again by typing 'help'
-            Console.WriteLine("Your job is to nurture it back to health and repopulate PodLandia.");
-            Console.WriteLine("Luckily, Podi reproduce by podicular parthenogenesis.");
-            Console.WriteLine("A happy and healthy Podus produces podicular protuberances, or *Podulates*");
-            Console.WriteLine("Once a particular number of protuberances (pods) appear, the Podus *Popates*,");
-            Console.WriteLine("and each pod becomes a new Podus, or Pop.");
-            Console.WriteLine("All the Pops will do exactly the same as the parent Podus");
-            Console.WriteLine("and, in time, will popate themselves.");
+            Console.WriteLine("As the universe's best xenobiologist, you have been called here to save the species!");
+            Console.WriteLine("Your job is to nurture the last Bloop back to health and happiness,");
+
+            Console.WriteLine("A happy and healthy Bloop with enough energy begins to bud.");
+            Console.WriteLine("Once three buds appear, and all are filled with energy,");
+            Console.WriteLine("the buds blow up and are released by the Bloop, leaving the Bloop and its ");
+            Console.WriteLine("three baby Bleeps tired, but ready to start the cycle again.");
+            Console.WriteLine("Because the Bleeps are all psychically connected, each will do exactly the same as the parent Bloop");
+            Console.WriteLine("and, in time, will bud themselves, blow up, and breed more Bleeps.");
             Console.WriteLine("If you take care of them, soon the planet will be repopulated!");
+            Console.WriteLine("Bleeps are known to be fun-loving and gentle creatures, but beware!");
+            Console.WriteLine("If you annoy them too much, they will be bothered, and will send you away.");
             //NTS-end moved section
 
             Console.WriteLine("Are you ready to begin?(yes/no)");
@@ -37,26 +49,72 @@ namespace virtualPet
             }
 
             //starting values
-            //pod = tentacle; pop = offspring; Podus = individual entity; Podi = name of species collectively
-            //original Podus and all poppys begin with 0 pods
-            
-            int numPops = 3; //number of offspring (pops) from a single popation
-            int totalPops = 1; //total number of original plus all offspring and grand offspring and great-grand...etc.
-            int popsToRePopulate = 100;//number of podi needed to repopulate the planet
 
 
-            while (totalPops < popsToRePopulate)
+            int numBuds = 3; //number of offspring (bleeps) from a single popation
+            int totalBleeps = 1; //total number of original plus all offspring and grand offspring and great-grand...etc.
+            int bleepsToRePopulate = 100;//number of podi needed to repopulate the planet
+
+
+            //ask to rename the Bloop
+
+            Console.WriteLine("The Bloop's name is Blobbi");
+            Console.WriteLine("Would you like to rename your Bloop?");
+            Console.WriteLine("1) yes");//starts out sad :(
+            Console.WriteLine("2) That's a great name! I'm glad to meet you, Blobbi!");//starts out happy :)!
+            int answerName = int.Parse(Console.ReadLine());
+            if (answerName == 1)
             {
-                //this is the main part of the program
-                //Eat() to get energy, Play() to be happy
-                //if energy (>=20), grows another pod (foot or tentacle)-->Podulate()
-                //if isHappy (true) and numPods (=3) and energy (>=20) then creates offspring -->Popate()
+                Console.WriteLine("What would you like to call your Bloop?");
+                //NTS name can't be defined here, pass from the class...
+                //NTS set isHappy to false - or just pass it from the definition??
+                //NTS invoke Bleeps(bloopName)
             }
-            //you win!
+            else
+            {
+                //set isHappy is true
+                //invoke Bleeps()
+            }
 
-            Console.WriteLine("Congratulations! There are now {0} Podi. You have repopulated Planet Podlandia!", totalPops);
+            //INTERFACE (shows each time)
+            //Example - Horace the Hippo
+            //Hunger: 27
+            //Thirst: 5
+            //Boredom: 5
+            //Tiredness: 50
 
-        /////////// here there be dragons - don't delete these 3 curly brackets!
-    }
+            //What do you want to do?
+            //1.Feed Horace
+            //2.Water Horace
+            //3.Play with Horace
+            //4.Put Horace to sleep
+            //5.Do nothing
+
+            Console.WriteLine("{0} the Bloop",Bleeps.bloopName);//need to pass this from the class?
+            Console.WriteLine("Energy: {0}",);
+
+
+            /*NTS - this is the advanced stuff - finish the MVP first!
+                        while (totalBleeps < bleepsToRePopulate)
+                        {
+                            //this is the main part of the program
+
+                            //show menu - either three different menus based on what is available, or one master menu and respond to user n/a
+                            //Eat() to get energy, Play() to be happy
+                            //if energy (>=20), grows another bud Bud()
+                            //if isHappy (true) and numBuds (=3) and energy (>=20) then creates offspring -->BlowsUp()
+
+
+
+
+                        }
+                        //you win!
+
+                        Console.WriteLine("Congratulations! There are now {0} Bleeps. You have saved the species!", totalBleeps);
+                        */
+
+
+            /////////// here there be dragons - don't delete these 3 curly brackets!
+        }
     }
 }
